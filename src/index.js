b310b8b01cd7a86d6c38541a0f9b4e7f15d7e4f2
multiplaie <multiplaie@protonmail.com>
2021-10-29 14:42:22 +0200

    implemente api control vlc

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ const express = require('express');
 const app = express();
 require('./models/dbConfig');
 const sectionRoutes = require('./routes/sectionController');
+const sectionRvlcRoutesoutes = require('./routes/vlcController');
 const config = require("../config.json");
 
 
 app.use(express.json())
 app.use('/api/sections', sectionRoutes);
+app.use('/api/vlc', vlcRoutes)
 app.listen(config.app.port, () => console.log('Server started: '+config.app.port));
