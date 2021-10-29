4ea8e9071a220f27da0733926668cc6227054adf
multiplaie <multiplaie@protonmail.com>
2021-10-29 15:10:31 +0200

    fix vlcroute call

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 require('./models/dbConfig');
 const sectionRoutes = require('./routes/sectionController');
-const sectionRvlcRoutesoutes = require('./routes/vlcController');
+const vlcRoutes = require('./routes/vlcController');
 const config = require("../config.json");
 
 
